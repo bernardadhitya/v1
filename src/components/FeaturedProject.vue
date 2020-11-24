50dff4ffdc87d7fa6aa592dbe4a029edc259ecb9
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-11-24 12:26:49 +0700

    [Bernard] Add FeaturedProject component

---
+++ src/components/FeaturedProject.vue
@@ -0,0 +1,106 @@
+<template>
+  <div>
+    <b-container>
+      <b-row>
+        <b-col>
+          <div class="image-wrapper">
+            <img src="../assets/images/homerooom-thumbnail.png" width="100%"/>
+          </div>
+        </b-col>
+      </b-row>
+      <b-row>
+        <b-col cols="2">
+          <div class="featured-project-number color-purple">01.</div>
+        </b-col>
+        <b-col cols="10">
+          <b-row>
+            <b-col>
+              <h5 class="color-purple left-align">Featured Project</h5>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <h2 class="left-align">Homerooom</h2>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <div class="card-wrapper">
+                <p class="left-align">
+                  An all-in-one mobile app solution for online learning. Made with React Native and Firebase
+                </p>
+              </div>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <h6 class="right-align">Techs used:</h6>
+              <p class="color-purple right-align">React Native, Firebase</p>
+            </b-col>
+          </b-row>
+          <b-row>
+            <b-col>
+              <div class="button-wrapper">
+                <b-btn variant="outline-primary">Go to repository</b-btn>
+              </div>
+            </b-col>
+          </b-row>
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeaturedProject',
+};
+</script>
+
+<style>
+  h2{
+    font-family: 'Bold';
+    font-size: 36px !important;
+  }
+  h5{
+    font-family: 'Medium';
+    font-size: 14px !important;
+  }
+  h6{
+    font-family: 'SemiBold';
+    font-size: 12px !important;
+  }
+  p{
+    font-family: 'Regular';
+    font-size: 16px !important;
+  }
+  .featured-project-number{
+    font-family: 'Regular';
+    font-size: 36px;
+  }
+  .color-purple{
+    color: #A55BF2;
+  }
+  .left-align{
+    text-align: left;
+  }
+  .right-align{
+    text-align: right;
+  }
+  .card-wrapper{
+    margin: 24px 0;
+    padding: 16px 16px 8px 16px;
+    border-radius: 16px;
+    box-shadow: 0px 2px 9px 4px rgba(216,216,216,0.5);
+    -webkit-box-shadow: 0px 2px 9px 4px rgba(216,216,216,0.5);
+    -moz-box-shadow: 0px 2px 9px 4px rgba(216,216,216,0.5);
+  }
+  .image-wrapper{
+    margin: 24px 0;
+    object-fit: contain;
+  }
+  .button-wrapper{
+    text-align: right;
+    margin-bottom: 40px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="home">
     <Introduction/>
+    <AboutMe/>
+    <FeaturedProject/>
+    <FeaturedProject/>
+    <FeaturedProject/>
   </div>
 </template>
 
 <script>
+import AboutMe from '../components/AboutMe.vue';
+import FeaturedProject from '../components/FeaturedProject.vue';
 // @ is an alias to /src
 import Introduction from '../components/Introduction.vue';
 
@@ -12,6 +18,8 @@ export default {
   name: 'home',
   components: {
     Introduction,
+    AboutMe,
+    FeaturedProject,
   },
 };
 </script>
