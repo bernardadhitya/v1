f15dfdb8ccb023a853abf8d6519fd060d462a204
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-12-19 15:47:58 +0700

    [Bernard] Add new featured project

--- src/constants/FeaturedProjects.js
+++ src/constants/FeaturedProjects.js
@@ -9,6 +9,14 @@ const featuredProjectsList = [
   },
   {
     id: '02',
+    title: 'pasarvisual',
+    description: 'A platform to connect creatives and business people, to help small business grow their business with creative digital media. Made with React Native and Firebase',
+    tags: ['React Native', 'Firebase'],
+    thumbnail: 'pasarvisual-thumbnail.jpg',
+    link: 'https://github.com/bernardadhitya/pasarvisual',
+  },
+  {
+    id: '03',
     title: 'intern-logbook-chatbot',
     description: 'Fill and view logbook from LINE. It also automatically updates local database to sync logbook faster, and autofill logbook',
     tags: ['Node.js', 'Github Actions', 'LINE API', 'Express.js'],
@@ -16,7 +24,7 @@ const featuredProjectsList = [
     link: 'https://github.com/bernardadhitya/intern-logbook-chatbot',
   },
   {
-    id: '03',
+    id: '04',
     title: 'imdb-puppeteer',
     description: 'This is a simple web scrapper for IMDb; creates a JSON API that makes IMDb data accessible for web applications with Puppeteer. Uses Vue.js for visualization',
     tags: ['Vue.js', 'Puppeteer', 'Node.js', 'Github Actions'],
@@ -24,7 +32,7 @@ const featuredProjectsList = [
     link: 'https://github.com/bernardadhitya/imdb-puppeteer',
   },
   {
-    id: '04',
+    id: '05',
     title: 'covid19-dashboard',
     description: 'A simple COVID-19 dashboard made with React.js; shows news and statistic of COVID-19 on designated country',
     tags: ['React.js', 'Chart.js', 'COVID-19 API', 'NewsAPI'],
